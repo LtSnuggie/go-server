e4aa20c5b1f0f968e1a8d0500eae2d93312d1aaa
Andrew Lintereur <andrew.lintereur@gmail.com>
2019-05-26 16:49:23 -0500

    Simple server working with test file

---
+++ server.go
@@ -0,0 +1,89 @@
+package server
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	communication "github.com/ltsnuggie/communication-service"
+	log "github.com/sirupsen/logrus"
+)
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+var routes = []Route{
+	// Route{
+	// 	"Get Player History",
+	// 	"GET",
+	// 	"/api/v1/lookup/{platform}/{name}",
+	// 	PlayerLookup,
+	// },
+}
+
+type srv struct {
+	router *mux.Router
+	port   string
+	server *http.Server
+}
+
+// var router *mux.Router
+var server *http.Server
+
+func New(port string) srv {
+
+	r := mux.NewRouter()
+
+	r.Use(communication.LoggingMiddleware)
+	log.WithFields(log.Fields{
+		"date": time.Now(),
+	}).Warn("Starting server...")
+
+	// These two lines are important if you're designing a front-end to utilise this API methods
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+	allowedHeaders := handlers.AllowedHeaders([]string{"Authorization"})
+	// Access-Control-Allow-Headers: Authorization
+
+	// Launch server with CORS validations
+	server = &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Addr:         ":" + port,
+		Handler:      handlers.CORS(allowedOrigins, allowedHeaders, allowedMethods)(r),
+	}
+	log.WithFields(log.Fields{
+		"date": time.Now(),
+	}).Warn("Server started...")
+	go server.ListenAndServe()
+
+	return srv{
+		router: r,
+		port:   port,
+		server: server,
+	}
+}
+
+func (s *srv) LoadEndpoint(name, path, method string, handlerFunc http.HandlerFunc) {
+	log.WithFields(log.Fields{
+		"method":   method,
+		"endpoint": path,
+		"date":     time.Now(),
+	}).Infof("%v end point loaded...", name)
+
+	s.router.
+		Methods(method).
+		Path(path).
+		Name(name).
+		Handler(handlerFunc)
+}
+
+func (s *srv) Stop() {
+	s.server.Close()
+}
---
+++ server_test.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"testing"
+	"time"
+)
+
+var testSrv srv
+var testText string
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+	testText = randSeq(144)
+}
+
+func TestGetServer(t *testing.T) {
+	testSrv = New("8077")
+}
+
+func TestEndpointBeforeLoad(t *testing.T) {
+	resp, err := http.Get("http://localhost:8077/test")
+	if err != nil {
+		t.Error(err)
+	}
+	expect := 404
+	if resp.StatusCode != expect {
+		t.Errorf("expected response status code of %d but received %d", expect, resp.StatusCode)
+	}
+}
+
+func TestLoadEndpoint(t *testing.T) {
+	testSrv.LoadEndpoint("test endpoint", "/test", "GET", testHandlerFunc)
+	resp, err := http.Get("http://localhost:8077/test")
+	if err != nil {
+		t.Error(err)
+	}
+	text, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	if err != nil {
+		t.Error(err)
+	}
+	if string(text) != testText {
+		t.Errorf("expected \"success\" but received %v", string(text))
+	}
+}
+func TestStopServer(t *testing.T) {
+	testSrv.Stop()
+}
+
+func testHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(testText))
+}
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
